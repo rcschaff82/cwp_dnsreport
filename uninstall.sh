b74aed8fa8af56fda7101f738ae0da995db0f3e0
rcschaff82 <30818412+rcschaff82@users.noreply.github.com>
2020-05-09 19:20:20 -0400

    Fixed Uninstall Script

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 \rm -Rf /usr/local/cwpsrv/htdocs/resources/admin/modules/dnsreport
+chattr -i /usr/local/cwpsrv/htdocs/admin/admin/
+chattr -i /usr/local/cwpsrv/htdocs/admin/admin/downloadreport.php
 \rm -f /usr/local/cwpsrv/htdocs/admin/admin/downloadreport.php
+chattr +i /usr/local/cwpsrv/htdocs/admin/admin/
 # Remove From Menu
 sd=$(grep -n "<\!-- cwp_dnsreport --" /usr/local/cwpsrv/htdocs/resources/admin/include/3rdparty.php | cut -f1 -d:)
 ed=$(grep -n "<\!-- end cwp_dnsreport --" /usr/local/cwpsrv/htdocs/resources/admin/include/3rdparty.php | cut -f1 -d:)
