f9d5968b50825abde0a6effb66bd2aa6e32ec49e
rcschaff82 <30818412+rcschaff82@users.noreply.github.com>
2020-05-09 20:13:19 -0400

    Fixed Menu Icon

--- install.sh
+++ install.sh
@@ -10,14 +10,14 @@ cat <<'EOF' >> /usr/local/cwpsrv/htdocs/resources/admin/include/3rdparty.php
 <!-- cwp_dnsreport -->
 <noscript>
 <li class="custom-menu"> <!-- this class "custom-menu" was added so you can remove the Developer Menu easily if you want -->
-    <a href="?module=dnsreport/index"><span class="icon16 icomoon-icon-chrome"></span>DNS Report</a>
+    <a href="?module=dnsreport/index"><span class="icon16 icomoon-icon-arrow-right-3"></span>DNS Report</a>
 </li>
 </noscript>
 <script type="text/javascript">
         $(document).ready(function() {
                 var newButtons = ''
                 +' <li>'
-                +' <a href="?module=dnsreport/index" class=""><span aria-hidden="true" class="icon16 icomoon-icon-right-arrow-3"></span>DNS Report</a>'
+                +' <a href="?module=dnsreport/index" class=""><span aria-hidden="true" class="icon16 icomoon-icon-arrow-right-3"></span>DNS Report</a>'
                 +'</li>';
                 $("ul#mn-12-sub").append(newButtons);
         });
